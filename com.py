c0f72e8fad6fe0e2fb1c1b5f72ecdd26cb9e521c
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-04-26 20:38:59 +0900

    不具合修正

--- com.py
+++ com.py
@@ -2,9 +2,10 @@ import subprocess
 import webbrowser
 import threading
 import psutil
+import os
 class Comand:
     def __init__(self):
-        self.pid=None
+        self.pid=[]
     def set_up(self):   
         webbrowser.open("http://127.0.0.1:5000/")
 
@@ -18,17 +19,23 @@ class Comand:
         
 
     def quit(self):
+        cwd=os.getcwd()
+        print(cwd)
         for proc in psutil.process_iter():
-            if 'python.exe'  in str(proc.exe) and 'app.py' in proc.cmdline()[1]:
-                self.pid=proc.pid
-            if self.pid  is not None:
-                command_quit=['taskkill','/pid',str(self.pid),'/F']
-                quit = subprocess.Popen(command_quit,shell=True)
-                quit.communicate()
-        
+            if 'python.exe'  in str(proc.exe) and cwd+'\\app.py' in proc.cmdline():
+                self.pid.append(proc.pid)
+            if self.pid != []:
+                for p in self.pid:
+                    command_quit = ['taskkill','/pid',str(p),'/F']
+                    quit = subprocess.Popen(command_quit,shell=True)
+                    quit.communicate()
+                    self.pid=[]
+                break
+            else:
+                continue
             
     def run_button_clicked(self):
-        run_thread=threading.Thread(target=self.run)
+        run_thread=threading.Thread(target=self.run,daemon=True)
         run_thread.start()
 
     def quit_button_clicked(self):
