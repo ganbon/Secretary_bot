f955944c52908e8a019f3955dacbc6a1118a3ca0
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-05-10 21:44:47 +0900

    通知機能追加

--- com.py
+++ com.py
@@ -3,22 +3,28 @@ import webbrowser
 import threading
 import psutil
 import os
+from system.notice import Notice
+
+
 class Command:
     def __init__(self):
-        self.pid=[]
+        self.pid = []
+        self.notice = Notice()
+        
     def set_up(self):   
         webbrowser.open("http://127.0.0.1:5000/")
 
-    def run(self):
+    def run(self, btn):
+        self.callback(btn)
         command_anaconda = ['activate','deep']
         command_python = ['python','app.py']
-        anaconda = subprocess.Popen(command_anaconda,shell=True)
+        anaconda = subprocess.Popen(command_anaconda, shell = True)
         anaconda.communicate()
-        python_file = subprocess.Popen(command_python,shell=True)
+        python_file = subprocess.Popen(command_python, shell = True)
         python_file.communicate()
         
-
-    def quit(self):
+    def quit(self, btn):
+        self.callback(btn)
         cwd=os.getcwd()
         for proc in psutil.process_iter():
             if 'python.exe'  in str(proc.exe) and cwd+'\\app.py' in proc.cmdline():
@@ -32,11 +38,33 @@ class Command:
                 break
             else:
                 continue
-            
-    def run_button_clicked(self):
-        run_thread=threading.Thread(target=self.run,daemon=True)
+    
+    def run_button_clicked(self, btn):
+        run_thread = threading.Thread(target=self.run,args=(btn,),daemon=True)
         run_thread.start()
 
-    def quit_button_clicked(self):
-        quit_thread=threading.Thread(target=self.quit)
+    def quit_button_clicked(self, btn):
+        quit_thread = threading.Thread(target=self.quit,args=(btn,))
         quit_thread.start()
+        
+    def notice_run(self, btn):
+        self.callback(btn)
+        self.notice.run()
+        
+    def notice_button_clicked(self, btn):
+        notice_thread = threading.Thread(target = self.notice_run, args = (btn,),daemon = True)
+        notice_thread.start()
+    
+    def run_app_clicked(self):
+        app_thread = threading.Thread(target = self.set_up)
+        app_thread.start()
+    
+    def callback(self, btn):
+        current_color = btn.cget('bg')
+        if current_color == "#fef4f4":
+            btn.config(bg = "#e6cde3")
+        else:
+            btn.config(bg = "#fef4f4")
+    
+
+    
--- set_up.py
+++ set_up.py
@@ -1,18 +1,21 @@
 import tkinter as tk
-import sys
 from com import Command
 
-com=Command()
-root=tk.Tk()
+com = Command()
+root = tk.Tk()
 root.title("秘書チャット")
-root.geometry("280x240")
-run_button=tk.Button(root,text="サーバ起動",width=30,command=com.run_button_clicked)
+root.geometry("350x300")
+root.config(bg='#eaf4fc')
+run_button = tk.Button(root, text="サーバ起動", width = 20, font = ('MSゴシック',17),
+                       fg = '#223a70', bg = '#fef4f4', command = lambda:com.run_button_clicked(run_button))
 run_button.pack()
-quit_button=tk.Button(root,text="サーバ停止",width=30,command=com.quit_button_clicked)
+quit_button = tk.Button(root, text = "サーバ停止", width = 20,font = ('MSゴシック',17),
+                        fg = '#223a70', bg = '#fef4f4', command = lambda:com.quit_button_clicked(run_button))
 quit_button.pack()
-app_button=tk.Button(root,text="アプリ起動",width=30,command=com.set_up)
+app_button = tk.Button(root, text = "アプリ起動", width = 20, font = ('MSゴシック',17),
+                       fg = '#223a70', bg = '#fef4f4', command = com.set_up)
 app_button.pack()
-
-
+notice_button = tk.Button(root, text="通知機能", width=20, font=('MSゴシック',17),
+                          fg = '#223a70', bg='#fef4f4', command = lambda:com.notice_button_clicked(notice_button))
+notice_button.pack()
 root.mainloop()
-
