002820bdd059f09c4025125a4e0cb97aef9fdf4e
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-05-27 23:37:50 +0900

    通知機能の不具合修正

--- com.py
+++ com.py
@@ -3,6 +3,7 @@ import webbrowser
 import threading
 import psutil
 import os
+import ctypes
 from system.notice import Notice
 
 
@@ -38,41 +39,38 @@ class Command:
         cwd = os.getcwd()
         pid_list = []
         for proc in psutil.process_iter():
-            if 'python.exe'  in str(proc.exe) and f'{cwd}\\{file_name}' in proc.cmdline():
+            if 'python.exe' in str(proc.exe) and f'{cwd}\\{file_name}' in proc.cmdline():
                 pid_list.append(proc.pid)
         return pid_list
     
     def app_switch(self, btn):
+        self.run_app_thread = threading.Thread(target = self.run)
+        self.quit_app_thread = threading.Thread(target = self.quit,args = ('app.py',))
         self.callback(btn)
         if btn.cget('bg') == '#fef4f4':
             btn.config(text = 'アプリ起動')
-            self.quit('app.py')
+            self.quit_app_thread.start()
         else:
             btn.config(text = 'アプリ停止')
-            self.run()
-        
-    def app_button_clicked(self, btn):
-        sv_app_thread = threading.Thread(target = self.app_switch, args = (btn,), daemon = True)
-        sv_app_thread.start()
+            self.run_app_thread.start()
         
     def notice_switch(self, btn):
         if btn.cget('bg') == '#e6cde3':
             btn.config(text = '通知ON')
             self.callback(btn)
-            
-            return
+            native_id = self.notice_thread.native_id
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(native_id, ctypes.py_object(SystemExit))
+            print("通知機能停止")
+            return   
         else:
+            self.notice_thread = threading.Thread(target = self.notice.run)
             self.callback(btn)
             btn.config(text = '通知OFF')
-            self.notice.run()
+            self.notice_thread.start()
         
-    def notice_button_clicked(self, btn):
-        notice_thread = threading.Thread(target = self.notice_switch, args = (btn,), daemon = True)
-        notice_thread.start()
-    
     def display_app_clicked(self):
-        app_thread = threading.Thread(target = self.set_up)
-        app_thread.start()
+        self.app_thread = threading.Thread(target = self.set_up)
+        self.app_thread.start()
     
     def callback(self, btn):
         current_color = btn.cget('bg')
