8a63e3b5789183e435a1e098631191fe283c3bf3
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-06-28 23:54:20 +0900

    詳細設定の箇所を修正

--- setting.py
+++ setting.py
@@ -5,18 +5,18 @@ def setting_save(setting_data,app):
     area = area.get()
     notice_step = notice_step.get()
     data = [month,day,voice,area,notice_step]
-    with open('setting.txt', mode = 'w', encoding = 'utf-8') as f:
+    with open('text_data/setting.txt', mode = 'w', encoding = 'utf-8') as f:
         for d in data:
             f.write(d+'\n')
     app.destroy()
 
 def setting_load():
-    with open('setting.txt', mode = 'r', encoding = 'utf-8') as f:
+    with open('text_data/setting.txt', mode = 'r', encoding = 'utf-8') as f:
         lines = f.readlines()
     for i,line in enumerate(lines):
         lines[i]=line.replace('\n','')
     return lines
         
-        
+
 
 
